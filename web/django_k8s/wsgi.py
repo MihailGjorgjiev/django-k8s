1ac50a2ec9cf6e5abd7319981b91129c927ad526
Mihail <gorgievmihail1@gmail.com>
2024-06-11 15:25:17 +0200

    added enviromental variables and docker

--- web/django_k8s/wsgi.py
+++ web/django_k8s/wsgi.py
@@ -8,8 +8,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import pathlib
+import dotenv
 from django.core.wsgi import get_wsgi_application
+
+CURR_DIR = pathlib.Path(__file__).resolve().parent
+BASE_DIR = CURR_DIR.parent
+ENV_FILE_DIR = BASE_DIR / ".env"
+
+dotenv.read_dotenv(str(ENV_FILE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_k8s.settings')
 
